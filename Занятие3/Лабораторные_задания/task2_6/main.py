c2254b062fcc8199d631c992cca6cdb7eb2d529f
Qyshasty <m-litv@yandex.ru>
2022-05-12 18:53:37 +0300

    test

---
+++ Занятие3/Лабораторные_задания/task2_6/main.py
@@ -0,0 +1,20 @@
+import json
+
+
+def task():
+    filename = "input.json"
+    with open(filename) as f:
+        json_data = json.load(f)
+
+    return sorted(json_data, key=lambda item: item["length"])
+
+
+if __name__ == "__main__":
+    data = task()
+    print(json.dumps(data, indent=4))
+
+    with open("output_file.json", "w") as f:
+         json.dump(json_data, f, indent=4)
+
+
+
